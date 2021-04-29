83bf408bf48e660230b5af924711f719408c9b7c
Martin Jakubik <martin@jakubik.info>
2021-04-29 23:04:05 +0200

    remove resource constants from tree data

--- scripts/treeConstants.gd
+++ scripts/treeConstants.gd
@@ -1,22 +1,6 @@
 extends Resource
 class_name TreeConstants
 
-# ENERGY
-
-export(float) var maxEnergyPerBranch
-export(float) var maxEnergyPerRoot
-export(float) var energyGainPerLeaf
-
-# WATER
-
-export(float) var maxWaterPerBranch
-export(float) var maxWaterPerRoot
-
-# STABILITY
-
-export(float) var stabilityPerBranch
-export(float) var stabilityPerRoot
-
 # ROOT GROWTH
 
 export(float) var minGrowthLength
@@ -24,8 +8,6 @@ export(float) var maxGrowthLength
 export(float) var maxGrowthAngle
 export(float) var maxBranchChildren
 export(float) var minRotationDifference
-export(float) var requiredEnergyForRootGrowth
-export(float) var requiredWaterForRootGrowth
 
 # BRANCH GROWTH
 
@@ -33,10 +15,6 @@ export(float) var branchGrowthChance
 export(float) var branchGrowthAmountMin
 export(float) var branchGrowthAmountMax
 export(float) var branchSpawnChance
-export(float) var requiredEnergyForBranchGrowth
-export(float) var requiredWaterForBranchGrowth
-export(float) var requiredEnergyForBranchChild
-export(float) var requiredWaterForBranchChild
 
 # GROUND
 
